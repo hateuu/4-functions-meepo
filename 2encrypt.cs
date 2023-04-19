d471f586c24a9052ec186f6759200f0a9cfafe10
hateuu <131145683+hateuu@users.noreply.github.com>
2023-04-20 01:42:35 +0700

    Add files via upload

--- 2encrypt.cs
+++ 2encrypt.cs
@@ -1,33 +1,37 @@
- 	static string EncryptByKey(string message, string key)
+﻿namespace ConsoleApp;
+
+public class encrypt2 {
+    public static string EncryptByKey(string message, string key)
+    {
+        try
         {
-            try
+            var charCodes = new Dictionary<char, char>();
+            for (int i = 0; i < key.Length; i++)
             {
-                var charCodes = new Dictionary<char, char>();
-                for (int i = 0; i < key.Length; i++)
-                {
-                    charCodes.Add(key[i], (char)(i + 65));
-                }
-
-                var encryptedChars = new List<char>();
-                foreach (char c in message)
-                {
-                    if (charCodes.TryGetValue(c, out char code))
-                    {
-                        encryptedChars.Add(code);
-                    }
-                    else
-                    {
-                        encryptedChars.Add(c);
-                    }
-                }
-
-                return new string(encryptedChars.ToArray());
-            }
-            catch 
-            {
-                Console.WriteLine("Не используйте два одинаковых символа для ключ-кода!");
-                 
+                charCodes.Add(key[i], (char)(i + 65));
             }
 
-            return null;
+            var encryptedChars = new List<char>();
+            foreach (char c in message)
+            {
+                if (charCodes.TryGetValue(c, out char code))
+                {
+                    encryptedChars.Add(code);
+                }
+                else
+                {
+                    encryptedChars.Add(c);
+                }
+            }
+
+            return new string(encryptedChars.ToArray());
         }
+        catch 
+        {
+            Console.WriteLine("Не используйте два одинаковых символа для ключ-кода!");
+                 
+        }
+
+        return null;
+    }
+}
