3c4114f66a88456264d04a8291851d3ce7872450
hateuu <131145683+hateuu@users.noreply.github.com>
2023-04-20 01:40:31 +0700

    Update 1encrypt.cs

--- 1encrypt.cs
+++ 1encrypt.cs
@@ -1,34 +1,38 @@
 //Первый способ шифрования
-        static string EncryptByFrequencyAnalysis(string message, string key)
+namespace ConsoleApp;
+
+public class encrypt1 {
+    public static string EncryptByFrequencyAnalysis(string message, string key)
+    {
+
+        try
         {
+            var charCodes = new Dictionary<char, string>();
+            for (int i = 0; i < key.Length; i++)
+            {
+                charCodes.Add(key[i], i.ToString());
+            }
 
-                try
+            var encryptedChars = new List<string>();
+            foreach (char c in message)
+            {
+                if (charCodes.TryGetValue(c, out string code))
                 {
-                    var charCodes = new Dictionary<char, string>();
-                    for (int i = 0; i < key.Length; i++)
-                    {
-                        charCodes.Add(key[i], i.ToString());
-                    }
-
-                    var encryptedChars = new List<string>();
-                    foreach (char c in message)
-                    {
-                        if (charCodes.TryGetValue(c, out string code))
-                        {
-                            encryptedChars.Add(code);
-                        }
-                        else
-                        {
-                            encryptedChars.Add(c.ToString());
-                        }
-                    }
-
-                    return string.Join(" ", encryptedChars);
+                    encryptedChars.Add(code);
                 }
-                catch
+                else
                 {
-                    Console.WriteLine("Не используйте два одинаковых символа для ключ-кода!");
+                    encryptedChars.Add(c.ToString());
                 }
+            }
 
-            return null;
+            return string.Join(" ", encryptedChars);
         }
+        catch
+        {
+            Console.WriteLine("Не используйте два одинаковых символа для ключ-кода!");
+        }
+
+        return null;
+    }
+}
