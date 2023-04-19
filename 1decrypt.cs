7ab3d104c1cdeea848d4302d4f93dde03a719ce8
hateuu <131145683+hateuu@users.noreply.github.com>
2023-04-20 01:39:59 +0700

    Update 1decrypt.cs

--- 1decrypt.cs
+++ 1decrypt.cs
@@ -1,24 +1,28 @@
-        static string DecryptByFrequencyAnalysis(string encryptedMessage, string key)
+namespace ConsoleApp;
+
+public class decrypt1 {
+    public static string DecryptByFrequencyAnalysis(string encryptedMessage, string key)
+    {
+        var charCodes = new Dictionary<string, char>();
+        for (int i = 0; i < key.Length; i++)
         {
-            var charCodes = new Dictionary<string, char>();
-            for (int i = 0; i < key.Length; i++)
-            {
-                charCodes.Add(i.ToString(), key[i]);
-            }
-
-            var encryptedChars = encryptedMessage.Split();
-            var decryptedChars = new List<string>();
-            foreach (var c in encryptedChars)
-            {
-                if (charCodes.TryGetValue(c, out char symbol))
-                {
-                    decryptedChars.Add(symbol.ToString());
-                }
-                else
-                {
-                    decryptedChars.Add(c);
-                }
-            }
-
-            return string.Join("", decryptedChars);
+            charCodes.Add(i.ToString(), key[i]);
         }
+
+        var encryptedChars = encryptedMessage.Split();
+        var decryptedChars = new List<string>();
+        foreach (var c in encryptedChars)
+        {
+            if (charCodes.TryGetValue(c, out char symbol))
+            {
+                decryptedChars.Add(symbol.ToString());
+            }
+            else
+            {
+                decryptedChars.Add(c);
+            }
+        }
+
+        return string.Join("", decryptedChars);
+    }
+}
