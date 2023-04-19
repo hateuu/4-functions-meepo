4b0357c4f05df0c4a48faea8cce45adb4afe1bf4
hateuu <131145683+hateuu@users.noreply.github.com>
2023-04-19 19:03:17 +0700

    Add files via upload

---
+++ 1decrypt.cs
@@ -0,0 +1,24 @@
+        static string DecryptByFrequencyAnalysis(string encryptedMessage, string key)
+        {
+            var charCodes = new Dictionary<string, char>();
+            for (int i = 0; i < key.Length; i++)
+            {
+                charCodes.Add(i.ToString(), key[i]);
+            }
+
+            var encryptedChars = encryptedMessage.Split();
+            var decryptedChars = new List<string>();
+            foreach (var c in encryptedChars)
+            {
+                if (charCodes.TryGetValue(c, out char symbol))
+                {
+                    decryptedChars.Add(symbol.ToString());
+                }
+                else
+                {
+                    decryptedChars.Add(c);
+                }
+            }
+
+            return string.Join("", decryptedChars);
+        }
