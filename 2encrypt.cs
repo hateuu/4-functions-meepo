1e59c3f84dfce7c06076804b907d73de14c03846
hateuu <131145683+hateuu@users.noreply.github.com>
2023-04-19 19:02:04 +0700

    Add files via upload

---
+++ 2encrypt.cs
@@ -0,0 +1,33 @@
+ 	static string EncryptByKey(string message, string key)
+        {
+            try
+            {
+                var charCodes = new Dictionary<char, char>();
+                for (int i = 0; i < key.Length; i++)
+                {
+                    charCodes.Add(key[i], (char)(i + 65));
+                }
+
+                var encryptedChars = new List<char>();
+                foreach (char c in message)
+                {
+                    if (charCodes.TryGetValue(c, out char code))
+                    {
+                        encryptedChars.Add(code);
+                    }
+                    else
+                    {
+                        encryptedChars.Add(c);
+                    }
+                }
+
+                return new string(encryptedChars.ToArray());
+            }
+            catch 
+            {
+                Console.WriteLine("Не используйте два одинаковых символа для ключ-кода!");
+                 
+            }
+
+            return null;
+        }
